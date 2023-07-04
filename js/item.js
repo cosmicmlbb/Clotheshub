20a222e3dd03a703c1e917c8db1a49918dd6a9fc
clotheshub <137139055+cosmicmlbb@users.noreply.github.com>
2023-07-04 15:59:10 +0800

    Add files via upload

---
+++ js/item.js
@@ -0,0 +1,641 @@
+const items = [
+  ﻿{
+    name: "Turbo Hoodie",
+    image: "items/hoodie.jpg",
+    price: "500.00",
+    rating: 4.5,
+    category: "hoodie",
+    description: "High quality white hoodie that fits on the trend!",
+  },
+{
+    name: "White Jacket",
+    image: "items/jacket.jpg",
+    price: "700.00",
+    rating: 4.5,
+    category: "jacket",
+    description:"Cool white jacket that fits your style",
+  },
+{
+    name: "White Polo Shirt Unisex",
+    image: "items/polo.jpg",
+    price: "250.00",
+    rating: 4.6,
+    category: "polo",
+    description:"100% Cotton Polo Shirt",
+  },
+{
+    name: "Pastel Blue Sweater with Sun Print",
+    image: "items/sweater.jpg",
+    price: "500.00",
+    rating: 5,
+    category: "sweater",
+    description:"Comfy and soft Pastel Sweater",
+  },
+{
+    name: "Plain Cream Unisex Tee",
+    image: "items/tshirt.jpg",
+    price: 150.00,
+    rating: 4.5,
+    category: "tshirt",
+    description:"Plain unisex tree",
+  },
+{
+    name: "Green Hoodie",
+    image: "items/hoodie2.jpg",
+    price: "550.00",
+    rating: 5,
+    category: "hoodie",
+    description:"High Quality Hoodie",
+  },
+{
+    name: "Dark Blue Jacket",
+    image: "items/jacket2.jpg",
+    price: "750.00",
+    rating: 4.3,
+    category: "jacket",
+    description:"High Quality Dark Blue Jacket",
+  },
+{
+    name: "Cream Polo Shirt",
+    image: "items/polo2.jpg",
+    price: "250.00",
+    rating: 4.7,
+    category: "polo",
+    description:"Trendy Polo Shirt",
+  },
+{
+    name: "Cream sweater with print",
+    image: "items/sweater2.jpg",
+    price: "650.00",
+    rating: 5,
+    category: "sweater",
+    description:"Soft and Comfy Sweater",
+  },
+{
+    name: "Pastel Purple Tee",
+    image: "items/tshirt2.jpg",
+    price: "150.00",
+    rating: 4.7,
+    category: "tshirt",
+    description:"100% Cotton Tee",
+  },
+{
+    name: "Dark Green hoodie",
+    image: "items/hoodie3.jpg",
+    price: "695.00",
+    rating: 4.9,
+    category: "hoodie",
+    description:"100% High Quality Hoodie",
+  },
+{
+    name: "Maroon Jacket",
+    image: "items/jacket3.jpg",
+    price: "700.00",
+    rating: 4.5,
+    category: "jacket",
+    description:"High Quality Jacket",
+  },
+{
+    name: "Pink Polo shirt for Women",
+    image: "items/polo3.jpg",
+    price: "200.00",
+    rating: 4.7,
+    category: "polo",
+    description:"High Quality Polo shirt",
+  },
+{
+    name: "Macha Green sweater with print",
+    image: "items/sweater3.jpg",
+    price: "550.00",
+    rating: 4.5,
+    category: "sweater",
+    description:"Soft and Comfy Sweater",
+  },
+{
+    name: "Plain Blue Tee",
+    image: "items/tshirt3.jpg",
+    price: "150.00",
+    rating: 4.5,
+    category: "tshirt",
+    description:"100% Cotton Tee",
+  },
+{
+    name: "Cream Hoodie",
+    image: "items/hoodie4.jpg",
+    price: "700.00",
+    rating: 4.9,
+    category: "hoodie",
+    description:"High Quality Hoodie",
+  },
+{
+    name: "Black Stylish Jacket",
+    image: "items/jacket4.jpg",
+    price: "500.00",
+    rating: 4.4,
+    category: "jacket",
+    description:"High Quality Jacket",
+  },
+{
+    name: "Grey Polo Shirt",
+    image: "items/polo4.jpg",
+    price: "200.00",
+    rating: 4.6,
+    category: "polo",
+    description:"High Quality Polo Shirt",
+  },
+{
+    name: "Pastel Pink Sweater",
+    image: "items/sweater4.jpg",
+    price: "400.00",
+    rating: 4.9,
+    category: "sweater",
+    description:"Comfy and soft sweater",
+  },
+{
+    name: "Pastel Pink Tee",
+    image: "items/tshirt4.jpg",
+    price: "150.00",
+    rating: 4.5,
+    category: "tshirt",
+    description:"100% Cotton Tee",
+  },
+{
+    name: "Nike Black Hoodie",
+    image: "items/hoodie5.jpg",
+    price: "900.00",
+    rating: 4.9,
+    category: "hoodie",
+    description:"Class A Nike Hoodie",
+  },
+{
+    name: "Blue and Grey Jacket",
+    image: "items/jacket5.jpg",
+    price: "800.00",
+    rating: 5,
+    category: "jacket",
+    description:"Trendy Jacket Perfect for you",
+  },
+{
+    name: "White Polo shirt for Women",
+    image: "items/polo5.jpg",
+    price: "200.00",
+    rating: 4.3,
+    category: "polo",
+    description:"100% Cotton Polo Shirt",
+  },
+{
+    name: "Grey Sweater",
+    image: "items/sweater5.jpg",
+    price: "450.00",
+    rating: 4.7,
+    category: "sweater",
+    description:"Soft and Comfy Sweater",
+  },
+{
+    name: "House white tee",
+    image: "items/tshirt5.jpg",
+    price: "200.00",
+    rating: 4.2,
+    category: "tshirt",
+    description:"100% Cotton Tee",
+  },
+{
+    name: "Grey Nike hoodie",
+    image: "items/hoodie6.jpg",
+    price: "900.00",
+    rating: 4.9,
+    category: "hoodie",
+    description:"Class A Nike Hoodie",
+  },
+{
+    name: "Japanese White Jacket",
+    image: "items/jacket6.jpg",
+    price: "999.00",
+    rating: 5,
+    category: "jacket",
+    description:"High Quality Jacket from Japan",
+  },
+{
+    name: "Blue Polo Shirt for Women",
+    image: "items/polo6.jpg",
+    price: "200.00",
+    rating: 4.8,
+    category: "polo",
+    description:"100% Cotton Polo Shirt for Women",
+  },
+{
+    name: "Macha Green Sweater with Print",
+    image: "items/sweater6.jpg",
+    price: "600.00",
+    rating: 4.9,
+    category: "sweater",
+    description:"Soft and comfy sweater",
+  },
+{
+    name: "AC DC Crop top",
+    image: "items/tshirt6.jpg",
+    price: "250.00",
+    rating: 4.4,
+    category: "tshirt",
+    description:"Trendy Crop top",
+  },
+{
+    name: "Pink Puffy Jacket",
+    image: "items/jacket7.jpg",
+    price: "650.00",
+    rating: 4.5,
+    category: "jacket",
+    description:"High quality puffy jacket",
+  },
+{
+    name: "Black Polo Shirt",
+    image: "items/polo7.jpg",
+    price: "200.00",
+    rating: 5,
+    category: "polo",
+    description:"100% Cotton Polo Shirt",
+  },
+{
+    name: "Pastel Pink Sweater",
+    image: "items/sweater7.jpg",
+    price: "600.00",
+    rating: 4.9,
+    category: "sweater",
+    description:"Soft and Comfy Sweater",
+  },
+{
+    name: "Brooklyn Tee",
+    image: "items/tshirt7.jpg",
+    price: "395.00",
+    rating: 4.6,
+    category: "tshirt",
+    description:"100% Cotton tee",
+  },
+{
+    name: "Green Polo Shirt for Women",
+    image: "items/polo8.jpg",
+    price: "200.00",
+    rating: 4.5,
+    category: "polo",
+    description:"100% Cotton Polo Shirt",
+  },
+{
+    name: "Panda Sweater",
+    image: "items/sweater8.jpg",
+    price: "500.00",
+    rating: 4.9,
+    category: "sweater",
+    description:"Soft and Comfy Sweater",
+  },
+{
+    name: "Green Sun Shirt",
+    image: "items/tshirt8.jpg",
+    price: "150.00",
+    rating: 4.5,
+    category: "tshirt",
+    description:"100% Cotton Tee",
+  },
+{
+    name: "White Eagle Polo Shirt",
+    image: "items/polo9.jpg",
+    price: "199.00",
+    rating: 4.7,
+    category: "polo",
+    description:"100% Cotton Polo Shirt",
+  },
+{
+    name: "Peach Sweater",
+    image: "items/sweater9.jpg",
+    price: "555.00",
+    rating: 4.4,
+    category: "sweater",
+    description:"Soft and comfy sweater",
+  },
+{
+    name: "California Tee",
+    image: "items/tshirt9.jpg",
+    price: "199.00",
+    rating: 4.1,
+    category: "tshirt",
+    description:"100% Cotton Tee",
+  },
+{
+    name: "Creamy Polo Shirt",
+    image: "items/polo10.jpg",
+    price: "195.00",
+    rating: 4.5,
+    category: "polo",
+    description:"100% Cotton Polo Shirt",
+  },
+{
+    name: "Grey and Green Sweater",
+    image: "items/sweater10.jpg",
+    price: "595.00",
+    rating: 4.5,
+    category: "sweater",
+    description:"Soft and Comfy Sweater",
+  },
+{
+    name: "Los Angeles Tee",
+    image: "items/tshirt10.jpg",
+    price: "250.00",
+    rating: 4.6,
+    category: "tshirt",
+    description:"100% Cotton Tee",
+  },
+{
+    name: "Dark Blue Polo Shirt",
+    image: "items/polo11.jpg",
+    price: "260.00",
+    rating: 4.5,
+    category: "polo",
+    description:"High Quality polo shirt",
+  },
+{
+    name: "Black Nike Sweater",
+    image: "items/sweater11.jpg",
+    price: "899.00",
+    rating: 4.7,
+    category: "sweater",
+    description:"Class A Nike Sweater",
+  },
+{
+    name: "Pink California Tee",
+    image: "items/tshirt11.jpg",
+    price: "195.00",
+    rating: 4.9,
+    category: "tshirt",
+    description:"100% Cotton Tee",
+  },
+{
+    name: "White Blue Grey Polo Shirt",
+    image: "items/polo12.jpg",
+    price: "259.00",
+    rating: 4.6,
+    category: "polo",
+    description:"High Quality Polo Shirt",
+  },
+{
+    name: "Grey Nike Sweater",
+    image: "items/sweater12.jpg",
+    price: "999.00",
+    rating: 4.9,
+    category: "sweater",
+    description:"Class A Nike Sweater",
+  },
+{
+    name: "Black and white Tee",
+    image: "items/tshirt12.jpg",
+    price: "145.00",
+    rating: 4.5,
+    category: "tshirt",
+    description:"100% Cotton Tee",
+  },
+{
+    name: "Blue and Red Polo shirt",
+    image: "items/polo13.jpg",
+    price: "299.00",
+    rating: 4.4,
+    category: "polo",
+    description:"High Quality Polo Shirt",
+  },
+{
+    name: "Black and white Tee with colar",
+    image: "items/tshirt13.jpg",
+    price: "195.00",
+    rating: 4.1,
+    category: "tshirt",
+    description:"100% Cotton Tee",
+  },
+{
+    name: "Blue and White Stripes Polo Shirt",
+    image: "items/polo14.jpg",
+    price: "200.00",
+    rating: 4.2,
+    category: "polo",
+    description:"100% Cotton Polo Shirt",
+  },
+{
+    name: "Skinny Stripes Black Tee",
+    image: "items/tshirt14.jpg",
+    price: "150.00",
+    rating: 4.5,
+    category: "tshirt",
+    description:"100% Cotton Tee",
+  },
+{
+    name: "Brown Tee",
+    image: "items/tshirt15.jpg",
+    price: "150.00",
+    rating: 4.5,
+    category: "tshirt",
+    description:"100% Cotton Tee",
+  },
+{
+    name: "Cream Tee",
+    image: "items/tshirt16.jpg",
+    price: "150.00",
+    rating: 4.5,
+    category: "tshirt",
+    description:"100% Cotton Tee",
+  },
+{
+    name: "Dark Green Tee",
+    image: "items/tshirt17.jpg",
+    price: "150.00",
+    rating: 4.5,
+    category: "tshirt",
+    description:"100% Cotton Tee",
+  },
+
+];
+
+const poloItems = [];
+const jacketItems = [];
+const tshirtItems = [];
+const sweaterItems = [];
+const hoodieItems = [];
+
+items.forEach((item) => {
+  if (item.category === "polo") {
+    poloItems.push(item);
+  } else if (item.category === "jacket") {
+    jacketItems.push(item);
+  } else if (item.category === "tshirt") {
+    tshirtItems.push(item);
+  } else if (item.category === "sweater") {
+    sweaterItems.push(item);
+  } else if (item.category === "hoodie") {
+    hoodieItems.push(item);
+  }
+});
+
+let poloIndex = 0;
+let jacketIndex = 0;
+let tshirtIndex = 0;
+let sweaterIndex = 0;
+let hoodieIndex = 0;
+
+function showModal(item) {
+  const modalTitle = document.getElementById("itemModalLabel");
+  modalTitle.textContent = item.name;
+
+  const modalBody = document.getElementById("modal-body");
+  modalBody.innerHTML = `
+    <img src="${item.image}" alt="${item.name}" class="img-fluid">
+    <p><strong>Price:</strong> ₱${item.price}</p>
+    <p><strong>Rating:</strong> ${item.rating}</p>
+    <p><strong>Description:</strong> ${item.description}</p>
+    <button class="btn btn-primary">Buy Now</button>
+    <button class="btn btn-secondary">Add to Cart</button>
+  `;
+
+  const myModal = new bootstrap.Modal(document.getElementById("itemModal"));
+  myModal.show();
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  const itemElements = document.getElementsByClassName("item-holder");
+  const modals = document.getElementsByClassName("modal");
+
+  for (let i = 0; i < itemElements.length; i++) {
+    const itemElement = itemElements[i];
+    const modal = modals[i];
+
+    itemElement.addEventListener("click", () => {
+      const item = items[i];
+      showModal(item, modal);
+    });
+  }
+
+  const poloSection = document.getElementById("polo-section");
+  poloSection.addEventListener("click", () => {
+    if (poloItems.length > 0) {
+      showModal(poloItems[poloIndex]);
+      poloIndex = (poloIndex + 1) % poloItems.length;
+    }
+  });
+
+  const jacketSection = document.getElementById("jacket-section");
+  jacketSection.addEventListener("click", () => {
+    if (jacketItems.length > 0) {
+      showModal(jacketItems[jacketIndex]);
+      jacketIndex = (jacketIndex + 1) % jacketItems.length;
+    }
+  });
+
+  const tshirtSection = document.getElementById("tshirt-section");
+  tshirtSection.addEventListener("click", () => {
+    if (tshirtItems.length > 0) {
+      showModal(tshirtItems[tshirtIndex]);
+      tshirtIndex = (tshirtIndex + 1) % tshirtItems.length;
+    }
+  });
+
+  const sweaterSection = document.getElementById("sweater-section");
+  sweaterSection.addEventListener("click", () => {
+    if (sweaterItems.length > 0) {
+      showModal(sweaterItems[sweaterIndex]);
+      sweaterIndex = (sweaterIndex + 1) % sweaterItems.length;
+    }
+  });
+
+  const hoodieSection = document.getElementById("hoodie-section");
+  hoodieSection.addEventListener("click", () => {
+    if (hoodieItems.length > 0) {
+      showModal(hoodieItems[hoodieIndex]);
+      hoodieIndex = (hoodieIndex + 1) % hoodieItems.length;
+    }
+  });
+});
+
+function generateItemHTML(item) {
+  let ratingHTML = "";
+  if (item.rating !== undefined) {
+    ratingHTML = `
+      <span style="font-size: 0.7em; margin-top: 5px">
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-star-fill" viewBox="0 0 16 16">
+          <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/>
+        </svg>
+        ${item.rating}
+      </span>
+    `;
+  }
+
+  return `
+    <div class="col-lg-2 col-md-4 col-sm-6 col-6">
+      <div class="item-holder">
+        <img src="${item.image}" alt="${item.name}" class="item-image">
+        <h4 class="item-name d-flex justify-content-between">
+          <span>${item.name}</span>
+          ${ratingHTML}
+        </h4>
+        <div class="item-price text-start">₱${item.price}</div>
+      </div>
+    </div>
+  `;
+}
+
+function renderItems(items, containerId, sectionTitle) {
+  const container = document.getElementById(containerId);
+  let itemsHTML = "";
+  for (const item of items) {
+    if (sectionTitle === "PRODUCTS" || item.category === sectionTitle) {
+      itemsHTML += generateItemHTML(item);
+    }
+  }
+  
+  if (sectionTitle !== "PRODUCTS") {
+    container.innerHTML = `
+      <h3 style="text-transform: uppercase; margin-top: 100px; font-weight: bold;">${sectionTitle}</h3>
+      <div class="row">
+        ${itemsHTML}
+      </div>
+    `;
+  } else {
+    container.innerHTML = `
+      <div class="row">
+        ${itemsHTML}
+      </div>
+    `;
+  }
+}
+
+renderItems(items, "item-container", "PRODUCTS");
+renderItems(poloItems, "polo-section", "Polo");
+renderItems(jacketItems, "jacket-section", "Jacket");
+renderItems(tshirtItems, "tshirt-section", "Tshirt");
+renderItems(sweaterItems, "sweater-section", "Sweater");
+renderItems(hoodieItems, "hoodie-section", "Hoodie");
+
+const itemContainer = document.getElementById("item-container");
+const poloSection = document.getElementById("polo-section");
+const jacketSection = document.getElementById("jacket-section");
+const tshirtSection = document.getElementById("tshirt-section");
+const sweaterSection = document.getElementById("sweater-section");
+const hoodieSection = document.getElementById("hoodie-section");
+
+poloItems.forEach((item) => {
+  const poloItemHTML = generateItemHTML(item);
+  poloSection.insertAdjacentHTML("beforeend", poloItemHTML);
+});
+
+jacketItems.forEach((item) => {
+  const jacketItemHTML = generateItemHTML(item);
+  jacketSection.insertAdjacentHTML("beforeend", jacketItemHTML);
+});
+
+tshirtItems.forEach((item) => {
+  const tshirtItemHTML = generateItemHTML(item);
+  tshirtSection.insertAdjacentHTML("beforeend", tshirtItemHTML);
+});
+
+sweaterItems.forEach((item) => {
+  const sweaterItemHTML = generateItemHTML(item);
+  sweaterSection.insertAdjacentHTML("beforeend", sweaterItemHTML);
+});
+
+hoodieItems.forEach((item) => {
+  const hoodieItemHTML = generateItemHTML(item);
+  hoodieSection.insertAdjacentHTML("beforeend", hoodieItemHTML);
+});
